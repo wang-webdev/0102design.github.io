83d86691c140cbaa51f2a05b25ec81f4248d9044
0102Design-Wang <hb810@live.com>
2023-10-06 01:52:36 +0900

    add vuetify3

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,18 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify のインポートを追加 **********/
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// 以下を追加 **********/
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+// 以下のように .use(vuetify) を追加
+createApp(App).use(vuetify).mount('#app')
